6680f172c757ac9f0b576a6bbc71e60dc37d7dee
Robert Beekman <robert@matsimitsu.nl>
2011-02-02 20:15:10 +0100

    added show show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,14 @@
 class ShowsController < ApplicationController
 
+  def show
+    @show = Show.first(:conditions => { :tvdb_id => params[:tvdb_id]})
+    if @show
+      render :text => @show.to_json
+    else
+      render :nothing => true, :head => :not_found
+    end
+  end
+
   def season
     @show = Show.first(:conditions => { :tvdb_id => params[:tvdb_id]})
     if @show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Itrakt::Application.routes.draw do
 
   root :to => 'home#index'
 
-  match 'shows/:tvdb_id/season/:season_number/episodes' => 'shows#season'
+  resources :shows do
+    match '/:tvdb_id/season/:season_number/episodes' => 'shows#season'
+  end
 
   resources :users do
     collection do
