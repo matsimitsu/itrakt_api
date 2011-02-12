d86afacc00f88d11cff8999530aa4284bbdda14a
Robert Beekman <robert@matsimitsu.nl>
2011-02-12 20:00:18 +0100

    minor changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-env :PATH, '/home/itrakt/app/shared/bundle/ruby/1.8/bin:/usr/local/bin:$PATH'
+env :PATH, '/home/itrakt/app/shared/bundle/ruby/1.8/bin:/usr/local/bin:/usr/bin:$PATH'
 set :output, File.join(path, 'log/cron.log')
 
 #every 1.day, :at => '4:00 am' do
