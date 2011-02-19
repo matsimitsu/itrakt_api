a7152a4f2cddb9c74b231ac2ce3b1126e40f7247
Robert Beekman <robert@matsimitsu.nl>
2011-02-19 15:39:15 +0100

    Using the same update strategy as trakt is, only an hour later

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ set :output, File.join(path, 'log/cron.log')
 #every 1.day, :at => '4:00 am' do
 #end
 
-every 1.day, :at => '09:03 am' do
+every 1.day, :at => '09:45 am' do
   rake 'update:tvdb'
 end
 
