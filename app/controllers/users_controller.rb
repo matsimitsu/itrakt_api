e606f23b37e5817f5842f76d7a3faa7424a50987
Robert Beekman <robert@matsimitsu.nl>
2011-02-19 19:58:36 +0100

    removed passwords from controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   before_filter :set_username_and_password
 
   def watched
-    password =
     watched = Rails.cache.fetch("watched_#{@username}", :expires_in => 10.minutes) do
       Trakt::User::Watched.new(@username, @password).enriched_results.to_json
     end
@@ -11,7 +10,6 @@ class UsersController < ApplicationController
   end
 
   def calendar
-    password = @password || DEFAULT_PASSWORD
     calendar = Rails.cache.fetch("calendar_#{@username}", :expires_in => 10.minutes) do
       Trakt::User::Calendar.new(@username, @password).enriched_results.to_json
     end
@@ -19,7 +17,6 @@ class UsersController < ApplicationController
   end
 
   def library
-    password = @password || DEFAULT_PASSWORD
     library = Rails.cache.fetch("library_#{@username}", :expires_in => 10.minutes) do
       Trakt::User::Library.new(@username, @password).enriched_results.to_json
     end
