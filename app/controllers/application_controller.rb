2bd4c4fb069a55a0163dc137d8df4d8f410b6d00
Robert Beekman <robert@matsimitsu.nl>
2011-02-21 17:37:49 +0100

    tracking api usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery
+  before_filter :ping_gauges
+
+  def ping_gauges
+    url = 'http://gaug.es/track?'
+    url << "h[site_id]=#{GAUGES_SITE_ID}&"
+    url << "h[title]=#{controller_name}-#{action_name}&"
+    url << "h[title]=#{controller_name}-#{action_name}&"
+    url << "h[resource]=#{CGI::escape request.url}&"
+    url << "h[user_agent]=#{CGI::escape request.user_agent}&"
+    url << "h[unique]=1"
+    HTTPI.get(url)
+  end
 
   def external_url(url)
     return unless url
