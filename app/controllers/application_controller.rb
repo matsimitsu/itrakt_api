4d41314e69d56ba1a388f2b0f563c3a93071af7f
Robert Beekman <robert@matsimitsu.nl>
2011-02-21 17:40:01 +0100

    removed duplicate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
     url = 'http://gaug.es/track?'
     url << "h[site_id]=#{GAUGES_SITE_ID}&"
     url << "h[title]=#{controller_name}-#{action_name}&"
-    url << "h[title]=#{controller_name}-#{action_name}&"
     url << "h[resource]=#{CGI::escape request.url}&"
     url << "h[user_agent]=#{CGI::escape request.user_agent}&"
     url << "h[unique]=1"
