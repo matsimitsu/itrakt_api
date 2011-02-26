0a21ab918a1322d5d4a3e5b706457e7b3092b58b
Robert Beekman <robert@matsimitsu.nl>
2011-02-26 19:36:32 +0100

    added username to cache key, since data is now personalized

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -12,7 +12,7 @@ class ShowsController < ApplicationController
 
   def seasons
     tvdb_id = params[:tvdb_id]
-    result = Rails.cache.fetch("seasons_#{tvdb_id}", :expires_in => 1.days) do
+    result = Rails.cache.fetch("seasons_#{tvdb_id}_#{@username}", :expires_in => 1.days) do
       Trakt::Show::Seasons.new(@username, @password, tvdb_id).enriched_results.to_json
     end
     render :text => result
@@ -21,7 +21,7 @@ class ShowsController < ApplicationController
   def season
     tvdb_id = params[:tvdb_id]
     season_number = params[:season_number]
-    result = Rails.cache.fetch("season_#{tvdb_id}_#{season_number}", :expires_in => 1.days) do
+    result = Rails.cache.fetch("season_#{tvdb_id}_#{season_number}_#{@username}", :expires_in => 1.days) do
       Trakt::Show::Season.new(@username, @password, tvdb_id, season_number).enriched_results.to_json
     end
     render :text => result
@@ -29,7 +29,7 @@ class ShowsController < ApplicationController
 
   def seasons_with_episodes
     tvdb_id = params[:tvdb_id]
-    result = Rails.cache.fetch("season_with_episodes_#{tvdb_id}", :expires_in => 1.day) do
+    result = Rails.cache.fetch("season_with_episodes_#{tvdb_id}_#{@username}", :expires_in => 1.day) do
       Trakt::Show::SeasonsWithEpisodes.new(@username, @password, tvdb_id).enriched_results.to_json
     end
     render :text => result
